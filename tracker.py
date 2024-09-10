488181b6ded488a1d83f2f894f7b36fa1d673ef6
Yajanth <yajanthrr@gmail.com>
2024-09-11 05:16:11 +0530

    model dev

---
+++ tracker.py
@@ -0,0 +1,29 @@
+def main():
+    print(f"***Running expense tracker:")
+
+    #Choose the options below
+    get_user_expense()
+
+
+    #write those to csv
+    write_expense_to_file()
+
+
+
+    #read file and summarize the expense
+    summarize_expenses()
+
+
+def get_user_expense():
+    print("Getting user expense:")
+
+def write_expense_to_file():
+    print("Updating the expenses:")
+
+def summarize_expenses():
+    print("summary of the expenses:")
+
+
+
+if __name__ == "__main__":
+    main()
